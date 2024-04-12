dbe19b259d039eb675984a66938739a3ddcc7659
DESKTOP-6T2G5CE\Abc <muhammadharis100555@gmail.com>
2024-04-13 01:21:37 +0500

    This is Trial Balance

---
+++ Trial_Balance.sql
@@ -0,0 +1,89 @@
+drop procedure if Exists `PROC_TRIAL_BALANCE_NEW`;
+
+DELIMITER $$
+CREATE PROCEDURE `PROC_TRIAL_BALANCE_NEW`(P_ACCOUNT_ID TEXT,
+									  P_ACCOUNT_TYPE TEXT, 
+								      P_ENTRY_DATE_FROM TEXT,
+									  P_ENTRY_DATE_TO TEXT,
+								      P_START INT,
+								      P_LENGTH INT,
+								      P_COMPANY_ID INT,
+									  P_YEAR TEXT )
+BEGIN
+
+
+    IF P_ACCOUNT_ID = "" THEN
+		SET P_ACCOUNT_ID = '-1';
+    END IF;
+    
+	IF P_ACCOUNT_TYPE = "" THEN
+		SET P_ACCOUNT_TYPE = '-1';
+    END IF;
+	
+	IF P_COMPANY_ID = "" Then 
+		SET P_COMPANY_ID = "-1";
+	END if;
+    
+        
+	
+	 SET @QRY = CONCAT('
+				Select 
+						
+						I.ACC_ID,
+						I.Description,
+						SUM(I.Debit) as Debit ,
+						SUM(I.Credit) as Credit,	
+						I.AccountId
+				from(
+						select 
+								
+								B.AccountId,
+								Case when D.Account_Id = "3" OR D.Account_Id = "2" OR D.Account_Id = "5" then B.Balance else 0 end as Debit,
+								Case when D.Account_Id = "1" OR D.Account_Id = "4" OR D.Account_Id = "6" then B.Balance else 0 end as Credit,
+								C.ACC_ID,
+								C.Description
+						from (	
+								select 
+										SUM(A.Balance) as Balance,
+										A.AccountId 
+								from 
+										dailyaccountbalance A 
+								where 
+										case when "',P_ENTRY_DATE_TO,'" <> -1 then A.ENTRYDATE <= DATE("',P_ENTRY_DATE_TO,'") else true end									
+								group by 
+										A.AccountId
+							 )B
+						inner join 
+									Accounts_Id C 
+						ON 
+									C.id = B.AccountId
+						inner join
+									Account_Type D 
+						ON	
+									D.id = C.Account_Type_Id
+						where 
+									case when ',P_COMPANY_ID,' <> -1 then C.Company_Id =',P_COMPANY_ID,' else true end
+						AND 
+									case when ',P_ACCOUNT_ID,' <> -1 then C.id =',P_ACCOUNT_ID,' else true end
+						AND 
+									case when ',P_ACCOUNT_TYPE,' <> -1 then D.id =',P_ACCOUNT_TYPE,' else true end
+
+					) I 
+				group by 
+						I.AccountId,
+						I.ACC_ID,
+						I.Description
+				with ROLLUP having I.AccountId is null OR I.Description is not null LIMIT ',P_START,',',P_LENGTH,';');
+				
+		
+			PREPARE STMP FROM @QRY;
+			EXECUTE STMP ;
+			DEALLOCATE PREPARE STMP;
+
+	
+
+
+
+
+END $$
+DELIMITER ;
